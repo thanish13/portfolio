bcab01e88c22df4b27d4c7ccd2d8823c292b4828
thanish13 <thanishmalai13@gmail.com>
2025-02-02 21:37:59 +0530

    Adding about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -86,7 +86,7 @@ const About = ({ delayAnimation, performanceMode }) => {
 						{/* Content */}
 						<motion.p variants={text}>
 							{
-								"I'm a 24 year old student from Finland, who enjoys creating fun projects around my passions. With a background in graphic design, I focus on the design and try to make everything look as good as possible."
+								"I'm a 24 year old developer from India, who enjoys creating fun projects around my passions. With a background in graphic design, I focus on the design and try to make everything look as good as possible."
 							}
 						</motion.p>
 						<motion.p variants={text}>
